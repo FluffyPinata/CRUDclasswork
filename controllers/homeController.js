b64a3d8ad5a2030d0060b877712c4be2ce638d75
jonesga1 <gabriel.jones@ucdenver.edu>
2021-04-08 15:44:31 -0600

    Final commit

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var courses = [
     {
         title: "Raspberry Cake",
@@ -14,22 +16,14 @@ var courses = [
 
 ]
 
-exports.showCourses = (req,res) => {
-    res.render("courses", {offeredCourses: courses});
-
-}
-
-exports.showSignUp = ( req, res) => {
-    res.render("contact")
-
-}
-
-exports.postedSignUpForm = ( req, res) => {
-    res.render("thanks")
-    
-}
-
-exports.showIndex = (req,res) => {
-    res.render("index");
-
+module.exports = {
+    index: (req, res) => {
+        res.render("index"); 
+    },
+    contact: (req,res) => {
+        res.render("contact");
+    },
+    courses: (req, res) => {
+        res.render("courses");
+    }
 }
--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,33 +1,109 @@
-const Subscriber = require("../models/subscriber");
+"use strict";
 
-exports.getAllSubscribers = (req, res) => {
-	Subscriber.find({})
-		.exec()
-		.then(subscribers => {
-			res.render("subscribers", {subscribers: subscribers})
-		})
-		.catch((error) => {
-			console.log(error);
-			return [];
-		})
-		.then(() => {
-			console.log("promise complete")
-		})
+const Subscriber = require("../models/subscriber"),
+getSubscriberParams = (body) => {
+    return {
+        name: body.name,
+        email: body.email,
+        zipCode: body.zipCode
+    };
 };
 
-exports.getSubscriptionPage = (req, res) => {
-	res.render("contact");
-};
+module.exports = {
+    index: (req,res,next) => {
+        Subscriber.find()
+        .then(subscribers => {
+            res.locals.subscribers = subscribers;
+            next()
+        })
+        .catch(error => {
+            console.log(`Error fetching subscriber data: ${error.message}`);
+            next(error);
+        });
+    },
+    indexView: (req, res) => {
+        res.render("subscribers/index");
+    },
+    new: (req, res) => {
+        res.render("subscribers/new");
+    },
+    create: (req, res, next) => {
+        let newSubscriber = new Subscriber({
+            name: req.body.name,
+            email: req.body.email,
+            zipCode: req.body.zipCode
+        });
+        Subscriber.create(newSubscriber)
+        .then( subscriber => {
+            res.locals.subscriber = subscriber;
+            res.locals.redirect = "/subscribers";
+            next();
+        })
+        .catch(error => {
+            console.log(`Error saving user ${error.message}`)
+            next(error)
+        });
+    },
+    redirectView: (req, res, rext) => {
+        let redirectPath = res.locals.redirect;
+        if(redirectPath) res.redirect(redirectPath);
+        else next();
+    },
+    show: (req, res, next) => {
+        let subscriberId = req.params.id;
+        Subscriber.findById(subscriberId)
+        .then(subscriber => {
+            res.locals.subscriber = subscriber;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error fetching subscriber by ID: ${error.message}`)
+        next(error);
+        });
+    },
+    showView: (req, res) => {
+        res.render("subscribers/show");
+    },
+    edit: (req, res) => {
+        let subscriberId = req.params.id;
+        Subscriber.findById(subscriberId)
+        .then(subscriber =>{
+            res.render("subscribers/edit", {subscriber: subscriber});
+        })
+        .catch(error => {
+            console.log(`Error fetching subscriber by ID: ${error.message}`);
+            next(error);
+        })
+    },
+    update: (req, res, next) => {
+        let subscriberId = req.params.id;
 
-exports.saveSubscriber = (req, res) => {
-	let newSubscriber = new Subscriber({
-		name: req.body.name,
-		email: req.body.email,
-		zipCode: req.body.zipCode
-	});
-	newSubscriber.save()
-	.then(() => {
-		res.render("thanks");
-	})
-	.catch(error => { res.send(error)});
-};
+        var updatedSubscriber = {};
+        updatedSubscriber.name = req.body.name;
+        updatedSubscriber.email = req.body.email;
+        updatedSubscriber.zipCode = req.body.zipCode;
+        
+        Subscriber.findByIdAndUpdate(subscriberId, updatedSubscriber)
+        .then(subscriber =>{
+            res.locals.subscriber = subscriber;
+            res.locals.redirect = `/subscribers/${subscriberId}`;
+            next();
+        })
+        .catch(error => {
+            console.log(`Error fetching subscriber by ID: ${error.message}`);
+            next(error);
+        })
+    },
+    delete: (req, res, next) => {
+        let subscriberId = req.params.id;
+        Subscriber.findByIdAndRemove(subscriberId)
+            .then(() => {
+                res.locals.redirect = "/subscribers";
+                next();
+            })
+            .catch(error => {
+                console.log(`Error fetching subscriber by ID: ${error.message}`);
+                next(error);
+            });
+    }
+}
